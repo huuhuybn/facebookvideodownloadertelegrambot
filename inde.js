320e7199d15dc4944b3d3b28022300b803654c5e
HuyNguyen <huuhuybn@gmail.com>
2023-04-22 13:10:30 +0700

    Initial commit

---
+++ inde.js
@@ -0,0 +1,25 @@
+const TelegramBot = require('node-telegram-bot-api')
+const token = '6263877560:AAEzXbCZvGJMqzo7Lv73XQtMcSomNRjIPUU'
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    console.log(msg)
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
